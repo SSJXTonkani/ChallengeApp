a7e954d92115177b985d68531370b0f2dcc49e32
SSJXTonkani <dawidsochax@gmail.com>
2024-03-01 16:38:12 +0100

    Zad z dnia 4 Easyfluj

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Bossman");
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+var name = "Antonina";
+var sex = "male";
+var age = 9;
+
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Wartość Nieprawidłowa");
+}
+
+
